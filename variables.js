a7db8c7e1ffe4da2bc64fecfb4072b1b96ef21d9
Meriem Abubeker <bintabibekr@gmail.com>
2024-04-12 22:38:32 +0300

    Adding in variables.js file

---
+++ variables.js
@@ -0,0 +1,17 @@
+// NUMBERS
+let x = 5
+console.log(x)
+
+let y = 5.5
+console.log(y)
+
+//STRING
+let z = "Hello"
+console.log(z)
+
+let a = "A string can be a whole sentence"
+console.log(a)
+
+//BOOLEAN
+let b = true
+console.log(b)
